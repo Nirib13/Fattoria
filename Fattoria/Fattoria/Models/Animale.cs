9e4aaa33b592110ca7a5673582f36429d37f19cf
andrea burgio <A.B.13@hotmail.it>
2018-04-08 17:37:39 +0200

    create classi

---
+++ Fattoria/Fattoria/Models/Animale.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fattoria.Models
+{
+    class Animale : IAnimale
+    {
+        public int IdAnimale { get; set; }
+        public string Nome { get; set; }
+        public int spazioOccupato { get; set; }
+        public void EmettiVerso(IAnimale animale)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Fattoria/Fattoria/Models/Fattoria.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fattoria.Models
+{
+    class Fattoria
+    {
+        List<Animale> listaAnimali = new List<Animale>();
+        public Fattoria()
+        {
+            listaAnimali.Add(new Mucca());
+            listaAnimali[0].IdAnimale = 1;
+            listaAnimali[0].Nome = "Carolina";
+            listaAnimali[0].spazioOccupato = 4;
+        }
+
+        internal List<Animale> ListaAnimali { get => ListaAnimali1; set => ListaAnimali1 = value; }
+        internal List<Animale> ListaAnimali1 { get => listaAnimali; set => listaAnimali = value; }
+    }
+}
